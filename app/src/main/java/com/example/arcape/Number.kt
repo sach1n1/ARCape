1550f91716da81ace7070689c4eb19e75b605f52
sach1n1 <sachin.s.dominic@gmail.com>
2022-01-15 23:40:28 +0100

    Adding Action Bar

--- app/src/main/java/com/example/arcape/Number.kt
+++ app/src/main/java/com/example/arcape/Number.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class Number : AppCompatActivity() {
 
@@ -19,7 +20,8 @@ class Number : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.number)
-
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
         val startBtn2 = findViewById<Button>(R.id.startBtn2)
         val startBtn3 = findViewById<Button>(R.id.startBtn3)
         val startBtn4 = findViewById<Button>(R.id.startBtn4)
